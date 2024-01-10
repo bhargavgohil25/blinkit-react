6f2cd710a180008e67de5fcf336ea75dab78a06b
bhargavgohil25 <bhargavgohil25@gmail.com>
2024-01-11 01:32:26 +0530

    fetch data

--- src/state/thunks/index.js
+++ src/state/thunks/index.js
@@ -10,14 +10,17 @@ import fetchJsonData from "../../api";
 
 export const fetchProducts = () => async (dispatch) => {
   dispatch(productsLoading());
-  const { products } = await fetchJsonData("/data/products.json");
+
+  const { products } = await fetchJsonData(
+    "https://run.mocky.io/v3/2943e73b-3f74-42c8-9e3a-54ff52385889"
+  );
   dispatch(productsLoaded(products));
 };
 
 export const fetchSideNavCategories = () => async (dispatch) => {
   dispatch(sideNavCategoriesLoading());
   const { sideNavCategories: categories } = await fetchJsonData(
-    "/data/sideNavCategories.json"
+    "https://run.mocky.io/v3/0fb7af1d-972d-41c7-a378-3075925c526b"
   );
   dispatch(sideNavCategoriesLoaded(categories));
 };
@@ -25,7 +28,7 @@ export const fetchSideNavCategories = () => async (dispatch) => {
 export const fetchSubHeaderCategories = () => async (dispatch) => {
   dispatch(subHeaderCategoriesLoading());
   const { subHeaderCategories: categories } = await fetchJsonData(
-    "/data/subHeaderCategories.json"
+    "https://run.mocky.io/v3/f65bddfe-5321-4660-bb9e-9b7f43093252"
   );
   dispatch(subHeaderCategoriesLoaded(categories));
 };
